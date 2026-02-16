958da508b9a7dae7bcf0669629d5cf284799dd35
Bridget <bridgetnamugga@gmail.com>
2026-02-16 22:17:53 +0300

    setup(backend):added user object to the request object

---
+++ backend/src/custom.d.ts
@@ -0,0 +1,11 @@
+import { Request } from 'express'; // This import might be needed depending on your setup
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any; 
+    }
+  }
+}
+
+export {}; 
