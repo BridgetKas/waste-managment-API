dd12167e09e5edba938c8037f0925b616511426d
Bridget <bridgetnamugga@gmail.com>
2026-02-16 22:16:00 +0300

    corsSetup(backend):Added cors to app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,23 +1,15 @@
 
 import express, {  type Application,type Request, type Response } from 'express';
 import userRouter  from './routes/user.route.ts';
-import dotenv from 'dotenv';
-
-// For environment variables
-dotenv.config();
+import cors from 'cors';
 
 export const app: Application = express();
-const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json())
-
-
 
 app.use(userRouter)
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
 
 
 
