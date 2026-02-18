59c78b1b003a7b0c87229336e87a3ab641e58159
Bridget <bridgetnamugga@gmail.com>
2026-02-18 13:55:21 +0300

    userController(backend):Added user controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,56 +1,46 @@
-import {type Request, type Response} from 'express'
-import User from '../models/user.model.ts';
-import bcrypt from 'bcryptjs';
-import jwt ,{ type JwtPayload} from 'jsonwebtoken'
-import dotenv from 'dotenv'
-
-dotenv.config({quiet:true})
-
-const secretKey = process.env.JWT_SECRET || 'your_secret_key'
-
-export const registerUsers = async (req:Request, res:Response) => {
-    const {firstName,lastName,email,password,registrationNumber,role} = req.body;
-
-    try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ firstName, lastName, email,registrationNumber,role, password: hashedPassword});
-        await user.save();
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (err:unknown) {
-        if (err instanceof Error) {
-            res.status(500).json({ message: 'Error registering user', error: err.message });
-        } else {
-            res.status(500).json({ message: 'An unexpected error occurred' });
-        }
-      }
-}
+import {type Request, type Response , type NextFunction} from 'express'
+import * as AuthService from '../services/auth.service.ts';
+import * as UserService from '../services/user.services.ts';
 
 
-export interface UserPayload extends JwtPayload {
-  firstName: string;
-  email: string;
-  role?: 'buyer' | 'seller'; 
-}
+export const registerUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user = await UserService.createUser(req.body);
+
+    return res.status(201).json({
+      success: true,
+      message: 'User registered successfully',
+    });
+
+  } catch (err: any) {
+    const statusCode = err.statusCode || 500;
+    
+    return res.status(statusCode).json({
+      success: false,
+      message: err.message || 'Internal Server Error'
+    });
+  }
+};
+
+
+export const authenticateUser = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+
+    const { token, user } = await AuthService.login(email, password);
+
+    return res.status(200).json({
+      success: true,
+      message: 'Logged in successfully',
+      token,
+      user
+    });
+  } catch (err: any) {
+    const statusCode = err.statusCode || 500;
+    return res.status(statusCode).json({
+      success: false,
+      message: err.message || 'Internal Server Error'
+    });
+  }
+};
  
-export const authenticateUser = async (req:Request, res:Response) => {
-    const {email,password} = req.body;
-
-    try {
-    const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: 'Invalid credentials' });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
-
-    const payload: UserPayload = { firstName: user.firstName, email: user.email };
-    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
-    res.status(200).json({ token, message: 'Logged in successfully' });
-    } catch (err:unknown) {
-        if (err instanceof Error) {
-                res.status(500).json({ message: 'Error registering user', error: err.message });
-            } else {
-                res.status(500).json({ message: 'An unexpected error occurred' });
-            }
-    }
-
-}
