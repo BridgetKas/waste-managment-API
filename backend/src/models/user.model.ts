5e8934201e1ada5c1c623289f5ce7a2b26736ca7
Bridget <bridgetnamugga@gmail.com>
2026-02-18 14:00:39 +0300

    user model(backend):Added user model

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,14 +1,17 @@
 import mongoose from 'mongoose';
-const { Schema } = mongoose;
+import { lowercase } from 'zod';
+const { Schema,model } = mongoose;
 
 const userSchema = new Schema({
   firstName: {
     type:String,
-    required:[true , 'First name is required']
+    required:[true , 'First name is required'],
+    trim:true
   }, 
   lastName: {
     type:String,
-    required:true
+    required:[true, 'Last name is required'],
+    trim:true
   }, 
   email: {
     type:String,
@@ -19,23 +22,34 @@ const userSchema = new Schema({
       },
       message: (props:any) => `${props.value} is not a valid email!`
     },
-    required:true
+    required:[true, 'Email is required'],
+    trim:true,
+    lowercase:true
   },
   password: {
     type:String,
-    required:true
+    required:[true, 'Password is required'],
+    select:false
   },
   registrationNumber: {
-    type:Number,
+    type:String,
     unique: true,
-    required:true
+    required:[true, 'Registration Number is requred']
   },
   role: {
     type:String,
-    required:true
+    required:true,
+    enum: {
+      values:['buyer', 'seller'],
+      message:'{VALUE} is not a supported role'
+    }, 
+    default: 'buyer',
+    
   } 
   
+},{ timestamps: true ,
+ 
 });
 
 
-export default  mongoose.model('User', userSchema);
+export   const User = model('User', userSchema);
