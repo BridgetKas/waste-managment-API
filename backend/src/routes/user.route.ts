d1e24f4007802c700174c5ea69ba905d83e45d37
Bridget <bridgetnamugga@gmail.com>
2026-02-18 14:03:30 +0300

    userRoutes:(backend):Added user routes

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -1,40 +1,29 @@
 import express, { Router } from 'express';
 import { z } from 'zod';
-import { validateUserData, validateUser} from '../middleware/user.middleware.ts';
-import { registerUsers, authenticateUser} from '../controllers/user.controller.ts';
+import { validate } from '../middleware/user.middleware.ts';
+import { registrationSchema,loginSchema } from '../schema/schema.ts';
+import { registerUsers , authenticateUser } from '../controllers/user.controller.ts';
 import { type Request, type Response,type NextFunction } from 'express';
 import { authMiddleware } from '../middleware/auth.middleware.ts';
 import { roleMiddleware } from '../middleware/role.middleware.ts';
 
 
-const userRouter: Router = express.Router();
+const router: Router = express.Router();
 
-const userSchema = z.object({
-  firstName: z.string().min(1, 'First name is required'),
-  lastName:z.string().min(1,'Last name is required'),
-  email:z.email(),
-  password:z.string(),
-  registrationNumber:z.number().int().positive('Registration Number must be positive'),
-  role:z.string()
-});
+// public routes
 
-const loginSchema = z.object({
-  email:z.email(),
-  password:z.string()
-
-}
-)
+router.post('/register', validate(registrationSchema),registerUsers);
+router.post('/login',validate(loginSchema),authenticateUser);
 
 
-userRouter.post('/register', validateUserData(userSchema),registerUsers);
-userRouter.post('/login',validateUser(loginSchema),authenticateUser);
-userRouter.get('/buyer', authMiddleware, (req:Request, res:Response) => {
+// protected routes
+router.get('/buyer', authMiddleware, (req:Request, res:Response) => {
   res.status(200).json({ message: 'Welcome to the buyer dashboard', user: req.user });
 });
 
-userRouter.get('/seller', [authMiddleware, roleMiddleware('seller')], (req:Request, res:Response) => {
+router.get('/seller', [authMiddleware, roleMiddleware('seller')], (req:Request, res:Response) => {
   res.status(200).json({ message: 'Welcome to the seller dashboard', user: req.user });
 });
 
 
-export default userRouter; 
+export default router; 
